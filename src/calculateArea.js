1a028bce8dde14b94dc0ac36c82e874902f50870
Sara Garcia <saragpwork95@gmail.com>
2024-10-09 18:58:29 +0100

    lab finished

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,12 @@
-function calculateArea() {
+function calculateArea(numA, numB) {
+    if (numA == undefined || numB == undefined) {
+        return undefined;
+    }
+    
+    if (typeof numA != "number" || typeof numB != "number") {
+        return undefined;
+    }
+
+    return numA * numB ;
 
 }
